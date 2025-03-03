fb8c925042868600716a81ac7106c51cee9c8b66
bondar-max <maksimusprime@gmail.com>
2025-03-03 10:22:26 +0700

    update

--- Range/src/ru/academits/java/bondar/range/Range.java
+++ Range/src/ru/academits/java/bondar/range/Range.java
@@ -1,5 +1,40 @@
 package ru.academits.java.bondar.range;
 
+import java.util.Scanner;
+
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        final double epsilon = 1e-10;
+
+        return from - number <= epsilon && number - to <= epsilon;
+    }
 }
 
--- Range/src/ru/academits/java/bondar/range_main/Main.java
+++ Range/src/ru/academits/java/bondar/range_main/Main.java
@@ -1,7 +1,31 @@
 package ru.academits.java.bondar.range_main;
 
+import ru.academits.java.bondar.range.Range;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Введите начало диапазона:");
+        double startNumber = scanner.nextDouble();
+
+        System.out.println("Введите конец диапазона:");
+        double endNumber = scanner.nextDouble();
+
+        Range range = new Range(startNumber, endNumber);
+
+        System.out.printf("Начало диапазона: %.2f%n", range.getFrom());
+        System.out.printf("Конец диапазона: %.2f%n", range.getTo());
+        System.out.printf("Длина диапазона: %.2f%n", range.getLength());
+
+        System.out.println("Введите число и узнайте принадлежит ли оно диапазону:");
+        double number = scanner.nextDouble();
+
+        if (range.isInside(number)) {
+            System.out.println("Число внутри диапазона");
+        } else {
+            System.out.println("Число вне диапазона");
+        }
     }
 }
