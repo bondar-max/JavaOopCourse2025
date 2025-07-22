a4de2086b52e38f8b209b88290fdc392e6dd388e
bondar-max <maksimusprime@gmail.com>
2025-07-22 10:37:02 +0700

    Changes in Main.java and Range.java

--- Range/src/ru/bondarmax/java/range/Range.java
+++ Range/src/ru/bondarmax/java/range/Range.java
@@ -34,37 +34,25 @@ public class Range {
     }
 
     public Range getSetsIntersection(Range range) {
-        double range1Start = range.getFrom();
-        double range1End = range.getTo();
+        double range1Start = this.getFrom();
+        double range1End = this.getTo();
 
-        double range2Start = this.getFrom();
-        double range2End = this.getTo();
+        double range2Start = range.getFrom();
+        double range2End = range.getTo();
 
-        if(range1End <= range2Start){ // первый диапазон полностью слева
+        if (range1End <= range2Start) { // первый диапазон полностью слева
             return null;
         }
 
-        if(range2End <= range1Start){ // второй диапазон полностью слева
+        if (range2End <= range1Start) { // второй диапазон полностью слева
             return null;
         }
 
         // определяем границы пересечения
-        double newStart;
-        double newEnd;
+        double newStart = Math.max(range1Start, range2Start);
+        double newEnd = Math.min(range1End, range2End);
 
-        if(range1Start < range2Start){
-            newStart = range2Start;
-        } else{
-            newStart = range1Start;
-        }
-
-        if(range1End <= range2End){
-            newEnd = range1End;
-        } else{
-            newEnd = range2End;
-        }
-
-        if(newStart >= newEnd){ // проверяем, что длина диапазона больше нуля
+        if (newStart >= newEnd) { // проверяем, что длина диапазона больше нуля
             return null;
         }
 
@@ -72,18 +60,18 @@ public class Range {
     }
 
     public Range[] getSetsUnion(Range range) {
-        double range1Start = range.getFrom();
-        double range1End = range.getTo();
+        double range1Start = this.getFrom();
+        double range1End = this.getTo();
 
-        double range2Start = this.getFrom();
-        double range2End = this.getTo();
+        double range2Start = range.getFrom();
+        double range2End = range.getTo();
 
         boolean intersects = this.isInside(range2Start) || this.isInside(range2End) ||
-                range.isInside(range1Start ) || range.isInside(range1End); // проверяем пересечения диапазонов
+                range.isInside(range1Start) || range.isInside(range1End); // проверяем пересечения диапазонов
 
-        if(intersects){ // диапазоны пересекаются
+        if (intersects) { // диапазоны пересекаются
             double minStart = Math.min(range1Start, range2Start);
-            double maxEnd = Math.max(range1End,range2End);
+            double maxEnd = Math.max(range1End, range2End);
 
             return new Range[]{new Range(minStart, maxEnd)};
         } else { // диапазоны не пересекаются
@@ -91,46 +79,46 @@ public class Range {
         }
     }
 
-    private Range[] getNewRange(double startPoint, double endPoint) {
-        return new Range[]{new Range(startPoint, endPoint)};
-    }
-
     public Range[] getSetsDifference(Range range) {
-        double range2Start = range.getFrom();
-        double range2End = range.getTo();
-
         double range1Start = this.getFrom();
         double range1End = this.getTo();
 
+        double range2Start = range.getFrom();
+        double range2End = range.getTo();
+
         Range[] result = new Range[2]; // массив для хранения до двух диапазонов результата
-        int count = 0;
+        int rangeQuantity = 0;
 
-        if(range1Start < range2Start && range1End > range2Start){ // проверяем левый участок
-            result[count] = new Range(range1Start, range2Start);
-            count++;
+        if (range1Start < range2Start && range1End > range2Start) { // проверяем левый участок
+            result[rangeQuantity] = new Range(range1Start, range2Start);
+            rangeQuantity++;
         }
 
-        if(range1End > range2End && range1Start < range2End){ // проверяем правый участок
-            result[count] = new Range(range2End, range1End);
-            count++;
+        if (range1End > range2End && range1Start < range2End) { // проверяем правый участок
+            result[rangeQuantity] = new Range(range2End, range1End);
+            rangeQuantity++;
         }
 
-        if(range1End <= range2Start) { // если первый диапазон полностью слева
-            result[count] = new Range(range1Start, range1End);
-            count++;
+        if (range1End <= range2Start) { // если первый диапазон полностью слева
+            result[rangeQuantity] = new Range(range1Start, range1End);
+            rangeQuantity++;
         }
 
-        if(range1Start >= range2End){ // если первый диапазон полностью справа
-            result[count] = new Range(range1Start , range1End);
-            count++;
+        if (range1Start >= range2End) { // если первый диапазон полностью справа
+            result[rangeQuantity] = new Range(range1Start, range1End);
+            rangeQuantity++;
         }
 
-        Range[] finalResult = new Range[count]; // создаём итоговый массив нужного размера
+        Range[] finalResult = new Range[rangeQuantity]; // создаём итоговый массив нужного размера
 
-        System.arraycopy(result, 0, finalResult, 0, count);
+        System.arraycopy(result, 0, finalResult, 0, rangeQuantity);
 
         return finalResult;
     }
+
+    public void printInformation(){
+        System.out.printf("%nНачало диапазона: %.2f%n", this.getFrom());
+        System.out.printf("Конец диапазона: %.2f%n", this.getTo());
+        System.out.printf("Длина диапазона: %.2f%n%n", this.getLength());
+    }
 }
-
-
--- Range/src/ru/bondarmax/java/range_main/Main.java
+++ Range/src/ru/bondarmax/java/range_main/Main.java
@@ -6,19 +6,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       /* Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите начало диапазона:");
+        System.out.println("Введите начало диапазона1:");
         double startNumber = scanner.nextDouble();
 
-        System.out.println("Введите конец диапазона:");
+        System.out.println("Введите конец диапазона1:");
         double endNumber = scanner.nextDouble();
 
         Range range = new Range(startNumber, endNumber);
 
-        System.out.printf("Начало диапазона: %.2f%n", range.getFrom());
-        System.out.printf("Конец диапазона: %.2f%n", range.getTo());
-        System.out.printf("Длина диапазона: %.2f%n", range.getLength());
+        range.printInformation();
 
         System.out.println("Введите число и узнайте принадлежит ли оно диапазону:");
         double number = scanner.nextDouble();
@@ -27,30 +25,57 @@ public class Main {
             System.out.println("Число внутри диапазона");
         } else {
             System.out.println("Число вне диапазона");
-        }*/
-
-        Range range = new Range(1, 10);
-
-        Range range2 = new Range(4, 8);
-
-        Range range3 = range.getSetsIntersection(range2);
-
-        if(range3!=null){
-            System.out.printf("Начало диапазона: %.2f%n", range3.getFrom());
-            System.out.printf("Конец диапазона: %.2f%n", range3.getTo());
         }
-        else{
+
+        System.out.printf("%nИзмените начало диапазона1:%n");
+        range.setFrom(scanner.nextDouble());
+
+        System.out.println("Измените конец диапазона1:");
+        range.setTo(scanner.nextDouble());
+
+        range.printInformation();
+
+        System.out.println("Введите начало диапазона2:");
+        startNumber = scanner.nextDouble();
+
+        System.out.println("Введите конец диапазона2:");
+        endNumber = scanner.nextDouble();
+
+        Range range2 = new Range(startNumber, endNumber);
+
+        System.out.printf("%nПересечение%n");
+
+        Range intersectionResult = range.getSetsIntersection(range2);
+
+        if (intersectionResult != null) {
+            System.out.printf("Начало диапазона: %.2f%n", intersectionResult.getFrom());
+            System.out.printf("Конец диапазона: %.2f%n", intersectionResult.getTo());
+
+            System.out.println();
+        } else {
             System.out.println("Нет пересечения");
         }
 
-        /*Range[] resultRanges = range.getSetsDifference(range2);
+        System.out.println("Объединение");
 
-        for (int i = 0; i < resultRanges.length; i++) {
-            System.out.printf("Начало диапазона%d: %.2f%n", i + 1, resultRanges[i].getFrom());
-            System.out.printf("Конец диапазона%d: %.2f%n", i + 1, resultRanges[i].getTo());
+        Range[] unionResult = range.getSetsUnion(range2);
+
+        for (int i = 0; i < unionResult.length; i++) {
+            System.out.printf("Начало диапазона%d: %.2f%n", i + 1, unionResult[i].getFrom());
+            System.out.printf("Конец диапазона%d: %.2f%n", i + 1, unionResult[i].getTo());
 
             System.out.println();
-        }*/
+        }
 
+        System.out.println("Разность");
+
+        Range[] differenceResult = range.getSetsDifference(range2);
+
+        for (int i = 0; i < differenceResult.length; i++) {
+            System.out.printf("Начало диапазона%d: %.2f%n", i + 1, differenceResult[i].getFrom());
+            System.out.printf("Конец диапазона%d: %.2f%n", i + 1, differenceResult[i].getTo());
+
+            System.out.println();
+        }
     }
 }
