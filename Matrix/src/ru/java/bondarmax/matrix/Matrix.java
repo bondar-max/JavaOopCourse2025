5eafd5495ba910f8844646a437ef9e41bc925b5e
bondar-max <maksimusprime@gmail.com>
2025-07-30 23:11:04 +0700

    update

--- Matrix/src/ru/java/bondarmax/matrix/Matrix.java
+++ Matrix/src/ru/java/bondarmax/matrix/Matrix.java
@@ -4,14 +4,14 @@ import ru.java.bondarmax.vector.Vector;
 
 public class Matrix {
     private final Vector[] rows;
-    private final int rowsCount;
-    private final int columnsCount;
+    private final int rowsQuantity;
+    private final int columnsQuantity;
 
 
     // Конструктор матрицы размера n x m, заполненной нулями
     public Matrix(int n, int m) {
-        rowsCount = n;
-        this.columnsCount = m;
+        rowsQuantity = n;
+        columnsQuantity = m;
         rows = new Vector[n];
 
         for (int i = 0; i < n; i++) {
@@ -21,27 +21,27 @@ public class Matrix {
 
     // Конструктор копирования
     public Matrix(Matrix other) {
-        rowsCount = other.rowsCount;
-        this.columnsCount = other.columnsCount;
-        rows = new Vector[rowsCount];
+        rowsQuantity = other.rowsQuantity;
+        columnsQuantity = other.columnsQuantity;
+        rows = new Vector[rowsQuantity];
 
-        for (int i = 0; i < rowsCount; i++) {
+        for (int i = 0; i < rowsQuantity; i++) {
             rows[i] = new Vector(other.rows[i]);
         }
     }
 
     // Конструктор из двумерного массива
     public Matrix(double[][] array) {
-        rowsCount = array.length;
-        this.columnsCount = array[0].length;
-        rows = new Vector[rowsCount];
+        rowsQuantity = array.length;
+        columnsQuantity = array[0].length;
+        rows = new Vector[rowsQuantity];
 
-        for (int i = 0; i < rowsCount; i++) {
+        for (int i = 0; i < rowsQuantity; i++) {
             rows[i] = new Vector(array[i]);
 
             // Дополняем нулями если нужно
-            if (rows[i].getSize() < this.columnsCount) {
-                double[] temp = new double[this.columnsCount];
+            if (rows[i].getSize() < columnsQuantity) {
+                double[] temp = new double[columnsQuantity];
                 System.arraycopy(array[i], 0, temp, 0, array[i].length);
                 rows[i] = new Vector(temp);
             }
@@ -50,20 +50,20 @@ public class Matrix {
 
     // Конструктор из массива векторов
     public Matrix(Vector[] vectors) {
-        int columnsCount = 0;
+        int  largestColumn = 0;
 
         for (Vector v : vectors) {
-            columnsCount = Math.max(columnsCount, v.getSize());
+            largestColumn = Math.max(largestColumn, v.getSize());
         }
 
-        rowsCount = vectors.length;
-        this.columnsCount = columnsCount;
-        rows = new Vector[rowsCount];
+        rowsQuantity = vectors.length;
+        columnsQuantity = largestColumn;
+        rows = new Vector[rowsQuantity];
 
-        for (int i = 0; i < rowsCount; i++) {
-            rows[i] = new Vector(columnsCount);
+        for (int i = 0; i < rowsQuantity; i++) {
+            rows[i] = new Vector(columnsQuantity);
 
-            for (int j = 0; j < columnsCount; j++) {
+            for (int j = 0; j < columnsQuantity; j++) {
                 if (j < vectors[i].getSize()) {
                     rows[i].setComponent(j, vectors[i].getComponent(j));
                 }
@@ -71,20 +71,20 @@ public class Matrix {
         }
     }
 
-    public int getRowsCount() {
-        return rowsCount;
+    public int getRowsQuantity() {
+        return rowsQuantity;
     }
 
-    public int getColumnsCount() {
-        return this.columnsCount;
+    public int getColumnsQuantity() {
+        return columnsQuantity;
     }
 
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder("{");
-        for (int i = 0; i < rowsCount; i++) {
+        for (int i = 0; i < rowsQuantity; i++) {
             result.append(rows[i].toString());
-            if (i < rowsCount - 1) {
+            if (i < rowsQuantity - 1) {
                 result.append(", ");
             }
         }
