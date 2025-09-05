61edbe82a83e06fa0082dc56302edee903e4aa1d
bondar-max <maksimusprime@gmail.com>
2025-09-05 12:20:12 +0700

    module ArrayList: update

--- ArrayList/src/ru/java/bondarmax/arraylist/ArrayList.java
+++ ArrayList/src/ru/java/bondarmax/arraylist/ArrayList.java
@@ -81,6 +81,7 @@ public class ArrayList<E> implements List<E>{
             return (T[]) Arrays.copyOf(internalStorage, currentSize);
         }
 
+        //noinspection SuspiciousSystemArraycopy
         System.arraycopy(internalStorage, 0, array, 0, currentSize);
 
         // Если исходный массив больше размера коллекции
@@ -91,29 +92,105 @@ public class ArrayList<E> implements List<E>{
         return array;
     }
 
-    @Override
-    public boolean add(E e) {
-        return false;
+    private void ensureCapacity(int requiredCapacity) {
+        if (requiredCapacity > internalStorage.length) {
+            increaseCapacity(requiredCapacity);
+        }
+    }
+
+    private void increaseCapacity(int requiredCapacity) {
+        int oldCapacity = internalStorage.length;
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+
+        if (newCapacity < requiredCapacity) {
+            newCapacity = requiredCapacity;
+        }
+
+        internalStorage = Arrays.copyOf(internalStorage, newCapacity);
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index > currentSize || index < 0) {
+            throw new IndexOutOfBoundsException("Индекс: " + index + ", Размер: " + currentSize);
+        }
     }
 
     @Override
-    public boolean remove(Object o) {
-        return false;
+    public boolean add(E element) {
+        ensureCapacity(currentSize + 1);
+        internalStorage[currentSize++] = element;
+        modificationCounter++;
+
+        return true;
     }
 
     @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
+    public boolean remove(Object element) {
+        int index = indexOf(element);
+
+        if (index == -1) {
+            return false;
+        }
+
+        removeElementAt(index);
+
+        return true;
+    }
+
+    private void removeElementAt(int index) {
+        int elementsToMove = currentSize - index - 1;
+
+        if (elementsToMove > 0) {
+            System.arraycopy(internalStorage, index + 1, internalStorage, index, elementsToMove);
+        }
+
+        internalStorage[currentSize - 1] = null; // Освобождаем память
+
+        currentSize--;
     }
 
     @Override
-    public boolean addAll(Collection<? extends E> c) {
-        return false;
+    public boolean containsAll(Collection<?> collection) {
+        for (Object element : collection) {
+            if (!contains(element)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
-    public boolean addAll(int index, Collection<? extends E> c) {
-        return false;
+    public boolean addAll(Collection<? extends E> collection) {
+        E[] newElements = collection.toArray((E[]) new Object[0]);
+        int newSize = newElements.length;
+
+        ensureCapacity(currentSize + newSize);
+        System.arraycopy(newElements, 0, internalStorage, currentSize, newSize);
+
+        currentSize += newSize;
+        modificationCounter++;
+
+        return newSize != 0;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> collection) {
+        if (index < 0 || index > currentSize) {
+            throw new IndexOutOfBoundsException("Индекс: " + index + ", Размер: " + currentSize);
+        }
+
+        E[] newElements = collection.toArray((E[]) new Object[0]);
+        int newSize = newElements.length;
+
+        ensureCapacity(currentSize + newSize);
+        System.arraycopy(internalStorage, index, internalStorage, index + newSize, currentSize - index);
+        System.arraycopy(newElements,0, internalStorage, index, newSize);
+
+        currentSize += newSize;
+        modificationCounter++;
+
+        return newSize != 0;
     }
 
     @Override
@@ -128,32 +205,83 @@ public class ArrayList<E> implements List<E>{
 
     @Override
     public void clear() {
+        for (int i = 0; i < currentSize; i++) {
+            internalStorage[i] = null;
+        }
 
+        currentSize = 0;
+        modificationCounter++;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        checkIndexBounds(index);
+        return internalStorage[index];
+    }
+
+    private void checkIndexBounds(int index) {
+        if (index > currentSize || index < 0) {
+            throw new IndexOutOfBoundsException("Индекс: " + index + ", Размер: " + currentSize);
+        }
+    }
+
+    private String getOutOfBoundsMessage(int index) {
+        return "Индекс: " + index + ", Размер: " + currentSize;
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        checkIndexBounds(index);
+
+        E oldValue = internalStorage[index];
+        internalStorage[index] = element;
+
+        modificationCounter++;
+
+        return oldValue;
     }
 
     @Override
     public void add(int index, E element) {
+        checkIndexForAdd(index);
+        ensureCapacity(currentSize + 1);
 
+        System.arraycopy(internalStorage, index, internalStorage, index + 1, currentSize - index);
+        internalStorage[index] = element;
+
+        currentSize++;
+        modificationCounter++;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        checkIndexBounds(index);
+
+        E oldValue = internalStorage[index];
+        removeElementAt(index);
+
+        modificationCounter++;
+
+        return oldValue;
     }
 
     @Override
-    public int indexOf(Object o) {
-        return 0;
+    public int indexOf(Object element) {
+        if (element == null) {
+            for (int i = 0; i < currentSize; i++) {
+                if (internalStorage[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < currentSize; i++) {
+                if (element.equals(internalStorage[i])) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
     }
 
     @Override
